9b1f37fce54ef77b11bb34d2a3f028afabea253c
Nitesh Prajapat <niteshp282000@gmail.com>
2022-02-28 20:33:05 +0530

    toggle bare for Login feature added!

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const navbar = document.querySelector(".navbar");
 const closeBtn = document.querySelector("#close");
 const searchIcon = document.querySelector("#search-icon");
 const searchForm = document.querySelector("#search-form");
+const loginBtn = document.querySelector(".login");
+const closeBtn2 = document.querySelector("#close2");
+const loginSection = document.querySelector("#login-section");
 
 
 menu.onclick = () =>{
@@ -28,4 +31,15 @@ closeBtn.onclick = () =>{
     searchForm.classList.remove("active")
 }
 
+loginBtn.onclick = () =>{
+    if(menu.classList.remove("fa-times") || navbar.classList.remove("active")){
+        menu.classList.remove("fa-times")
+        navbar.classList.remove("active")
+    }else{
+        loginSection.classList.toggle("active")
+    }
+}
 
+closeBtn2.onclick = () =>{
+    loginSection.classList.remove("active")
+}
